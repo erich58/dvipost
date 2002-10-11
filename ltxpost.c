4feb0f15d059a2a3038d83cf3390626f6c11233e
Erich Frühstück <none@none>
2002-10-11 06:40:41 +0200

    file handling

--- ltxpost.c
+++ ltxpost.c
@@ -5,8 +5,89 @@
 
 
 #include "dvipost.h"
+#include <unistd.h>
+#include <getopt.h>
+
+extern int optind;
+extern char *optarg;
+
+int verbose = 0;
+
+static void usage (const char *name)
+{
+	fprintf(stderr, "usage: %s [-v] input [output]\n", name);
+}
 
 int main (int argc, char **argv)
 {
+	FILE *input, *output;
+	int opt;
+	int c;
+
+	while ((opt = getopt(argc, argv, "v")) != -1)
+	{
+		switch (opt)
+		{
+		case 'v':
+			verbose++;
+			break;
+		default:
+			return EXIT_FAILURE;
+		}
+	}
+
+	if	(optind == argc)
+	{
+		usage(argv[0]);
+		return EXIT_SUCCESS;
+	}
+
+	if	(optind < argc)
+	{
+		if	(strcmp(argv[optind], "-") != 0)
+		{
+			input = fopen(argv[optind], "rb");
+
+			if	(!input)
+			{
+				fprintf(stderr, "%s: file %s not found.\n",
+					argv[0], argv[optind]);
+				return EXIT_FAILURE;
+			}
+		}
+		else	input = stdin;
+
+		optind++;
+	}
+
+	if	(optind < argc)
+	{
+		if	(strcmp(argv[optind], "-") != 0)
+		{
+			output = fopen(argv[optind], "wb");
+
+			if	(!output)
+			{
+				fprintf(stderr, "%s: file %s not found.\n",
+					argv[0], argv[optind]);
+				fclose(input);
+				return EXIT_FAILURE;
+			}
+		}
+		else	output = stdout;
+
+		optind++;
+	}
+	else	output = stdout;
+
+	if	(optind < argc)
+	{
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	while ((c = getc(input)) != EOF)
+		putc(c, output);
+
 	return EXIT_SUCCESS;
 }
